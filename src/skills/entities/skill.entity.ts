948c822a693f05b8e2ab9a4bf6367029b25b4f36
MurAd <muradisayevazer@gmail.com>
2023-03-10 13:50:23 +0400

    Skills table created, many-to-many relation established

--- src/skills/entities/skill.entity.ts
+++ src/skills/entities/skill.entity.ts
@@ -1,1 +1,14 @@
-export class Skill {}
+import { Project } from "src/projects/entities/project.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Skill {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @ManyToMany(() => Project, (project) => project.skills)
+    projects: Project[];
+}
