18bf5f4c35420eebaf2566ddc9adca28109415ff
MurAd <muradisayevazer@gmail.com>
2023-03-11 02:38:07 +0400

    DTO and Service added to projects API

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,27 @@
-export class CreateProjectDto {}
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsString, IsUrl, ValidateNested } from 'class-validator';
+
+class SkillDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+}
+
+export class CreateProjectDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl(undefined, { message: 'GitHub URL is not valid.' })
+  url: string;
+
+  @Type(() => SkillDto)
+  @ValidateNested({ each: true })
+  skills: SkillDto[];
+}
