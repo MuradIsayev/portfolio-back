e7540df2606d458a6c66c4b2e213d02230040770
MurAd <muradisayevazer@gmail.com>
2023-04-17 15:42:47 +0400

    few changes

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -3,19 +3,21 @@ import {
   SubscribeMessage,
   MessageBody,
   WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway {
-  @WebSocketServer() server;
+  @WebSocketServer() server: Server;
   constructor(private readonly chatService: ChatService) {}
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() body: CreateChatDto) {
+  async handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() body: CreateChatDto) {
+    console.log(socket.listenerCount('message'));
     this.server.emit('message', body.message);
-    console.log(body);
     return body;
   }
 
--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateChatDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
+  userName: string;
 
   @IsString()
   @IsNotEmpty()
