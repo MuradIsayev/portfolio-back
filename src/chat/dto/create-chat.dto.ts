9e1dac6bc28582b0aca9f2c49ed5bb94e623167e
MurAd <muradisayevazer@gmail.com>
2023-04-17 15:15:27 +0400

    dto for message body

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,1 +1,15 @@
-export class CreateChatDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateChatDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  message: string;
+
+  @IsString()
+  @IsNotEmpty()
+  photoUrl: string;
+}
