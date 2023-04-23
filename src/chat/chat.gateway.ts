21148ac897115f46087116168731adecefcf2c73
MurAd <muradisayevazer@gmail.com>
2023-04-23 17:46:52 +0400

    socket id is returned to client-side

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -3,10 +3,11 @@ import {
   SubscribeMessage,
   MessageBody,
   WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway {
@@ -14,13 +15,14 @@ export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
 
   @SubscribeMessage('initiate')
-  async create(@MessageBody() body: CreateChatDto) {
+  async create(@MessageBody() body: CreateChatDto, @ConnectedSocket() socket: Socket) {
     await this.chatService.create(body);
     console.log(`${body.userName} with uid ${body.uuid} has joined the guestbook`);
+    return this.server.emit('userJoined', socket.id);
   }
 
   @SubscribeMessage('message')
-  async handleMessage(@MessageBody() body: CreateChatDto) {
+  async handleMessage(@MessageBody() body: CreateChatDto,) {
     return await this.chatService.handleMessage(body);
   }
 
