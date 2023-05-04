a8448a2e14c07353db6da536495d6a704740e505
MurAd <muradisayevazer@gmail.com>
2023-05-04 21:42:16 +0400

    blog entity conf

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
 import { NotionModule } from 'nestjs-notion';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Blog } from './entities/blog.entity';
 
 @Module({
   imports: [
     NotionModule.forRoot({
       auth: 'secret_Ooy09BQepK2ewbBIBTjWlC5BU2vLVa6ivuMJPaJUrx2',
     }),
+    TypeOrmModule.forFeature([Blog]),
   ],
   controllers: [BlogsController],
   providers: [BlogsService],
