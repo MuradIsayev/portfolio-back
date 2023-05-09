2aeebbfa15e02406925a021726e20fb094574062
MurAd <muradisayevazer@gmail.com>
2023-05-09 19:11:49 +0400

    new endpoint added for tag search

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
+import { FilterTagDto } from 'src/tags/dto/filter-tag.dto';
 
 @Controller('blogs')
 export class BlogsController {
   constructor(private readonly blogsService: BlogsService) {}
+  @Get('search/')
+  findByTag(@Query('tag') filter: FilterTagDto) {
+    return this.blogsService.findFiltered(filter);
+  }
 
   @Get(':blockId')
   getBlogs(@Param('blockId') blockId: string) {
