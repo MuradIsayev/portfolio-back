c7d44e750c3d7416093ae7134cd95d2447753c23
MurAd <muradisayevazer@gmail.com>
2023-06-05 15:31:36 +0400

    new entities added

--- src/config/db.entities.ts
+++ src/config/db.entities.ts
@@ -1,6 +1,8 @@
 import { Blog } from 'src/blogs/entities/blog.entity';
+import { Experience } from 'src/experience/entities/experience.entity';
 import { Project } from 'src/projects/entities/project.entity';
 import { Skill } from 'src/skills/entities/skill.entity';
 import { Tag } from 'src/tags/entities/tag.entity';
+import { WorkSchedule } from 'src/work-schedule/entities/work-schedule.entity';
 
-export const entities = [Skill, Project, Blog, Tag];
+export const entities = [Skill, Project, Blog, Tag, Experience, WorkSchedule  ];
