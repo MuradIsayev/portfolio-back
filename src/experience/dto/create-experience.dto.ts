6c6219c2b358570ae17bd04d7c815bc3fb906f2c
MurAd <muradisayevazer@gmail.com>
2023-06-06 15:50:44 +0400

    create dto created

--- src/experience/dto/create-experience.dto.ts
+++ src/experience/dto/create-experience.dto.ts
@@ -1,1 +1,35 @@
-export class CreateExperienceDto {}
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateExperienceDto {
+  @IsString()
+  @IsNotEmpty()
+  position!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  company!: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Type(() => Number)
+  departmentId!: number;
+
+  @IsDateString()
+  @IsNotEmpty()
+  startedAt!: Date;
+
+  @IsDateString()
+  @IsOptional()
+  endedAt?: Date;
+}
