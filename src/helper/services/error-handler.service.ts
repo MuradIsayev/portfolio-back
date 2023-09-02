685039377c774b0f0912387f5f19b5807e29055f
MurAd <muradisayevazer@gmail.com>
2023-09-02 17:25:39 +0400

    checkError and checkDup functions added

--- src/helper/services/error-handler.service.ts
+++ src/helper/services/error-handler.service.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  ConflictException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -15,21 +16,47 @@ export class ErrorHandlerService {
     }
   }
 
-  public handleNotFoundException(entity: any, entityName: string) {
-    if (!entity) {
-      throw new NotFoundException({
-        code: 'ER_NOT_FOUND',
-        message: `${entityName} not found`,
-      });
-    }
-  }
-
   public checkEntityIsEmpty(entities: any, entityName: string) {
     if (!entities || !entities.length) {
       throw new BadRequestException({
         code: 'ER_NO_ENTITY',
         message: `${entityName} not found`,
       });
+    }
+  }
+
+  public async checkDuplication(error: any, entityName: string) {
+    //ToDo: Remove async
+    if (error.code === 'ER_DUP_ENTRY') {
+      throw new ConflictException(`${entityName} already exists`);
+    }
+
+    throw new BadRequestException('Something went wrong');
+  }
+
+  public checkError(error: any, entityName: string) {
+    const code = error?.code || error?.response?.code;
+
+    switch (code) {
+      case 'ER_DUP_ENTRY':
+        throw new ConflictException(`${entityName} already exists`);
+      case 'ER_NO_REFERENCED_ROW_2':
+        throw new ConflictException(`${entityName} can't be a foreign key`);
+      case 'ER_NO_ENTITY':
+        throw new BadRequestException(
+          error?.response?.message || `${entityName} not found`,
+        );
+      case 'ER_HAS_CHILDREN':
+        throw new BadRequestException(
+          error?.response?.message || `${entityName} has children entities`,
+        );
+      case 'ER_NOT_FOUND':
+        throw new NotFoundException(
+          error?.response?.message || `${entityName} not found`,
+        );
+      default:
+        console.log(error);
+        throw new BadRequestException('Something went wrong');
     }
   }
 }
