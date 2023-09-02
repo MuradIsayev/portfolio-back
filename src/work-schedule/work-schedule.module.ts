8f8f422f6a8552c4b35f4d195228af7936a6582a
MurAd <muradisayevazer@gmail.com>
2023-09-02 22:51:44 +0400

    Work Schedule Module updated

--- src/work-schedule/work-schedule.module.ts
+++ src/work-schedule/work-schedule.module.ts
@@ -3,9 +3,10 @@ import { WorkScheduleService } from './work-schedule.service';
 import { WorkScheduleController } from './work-schedule.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkSchedule } from './entities/work-schedule.entity';
+import { HelperModule } from '../helper/helper.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WorkSchedule])],
+  imports: [TypeOrmModule.forFeature([WorkSchedule]), HelperModule],
   controllers: [WorkScheduleController],
   providers: [WorkScheduleService]
 })
--- src/work-schedule/work-schedule.service.ts
+++ src/work-schedule/work-schedule.service.ts
@@ -3,20 +3,50 @@ import { CreateWorkScheduleDto } from './dto/create-work-schedule.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { WorkSchedule } from './entities/work-schedule.entity';
+import { ErrorHandlerService } from '../helper/services/error-handler.service';
 
 @Injectable()
 export class WorkScheduleService {
   constructor(
     @InjectRepository(WorkSchedule)
     private workScheduleRepository: Repository<WorkSchedule>,
+    private readonly errorHandlerService: ErrorHandlerService,
   ) {}
 
   async create(createWorkScheduleDto: CreateWorkScheduleDto) {
-    const workSchedule = this.create(createWorkScheduleDto);
-    return await this.workScheduleRepository.save(workSchedule);
+    try {
+      const workSchedule = this.create(createWorkScheduleDto);
+
+      return await this.workScheduleRepository.save(workSchedule);
+    } catch (e) {
+      await this.errorHandlerService.checkDuplication(
+        e,
+        `WorkSchedule ${createWorkScheduleDto.type}`,
+      );
+    }
   }
 
-  async findAll() {
+  async findAll(): Promise<WorkSchedule[]> {
     return await this.workScheduleRepository.find();
+  }
+
+  async findOne(id: number): Promise<WorkSchedule> {
+    const workSchedule: WorkSchedule =
+      await this.workScheduleRepository.findOneBy({ id });
+
+    this.errorHandlerService.checkEntity(workSchedule, `WorkSchedule ${id}`);
+
+    return workSchedule;
+  }
+
+  async remove(id: number) {
+    try {
+      const workSchedule: WorkSchedule = await this.findOne(id);
+      await this.workScheduleRepository.remove(workSchedule);
+
+      return true;
+    } catch (e) {
+      this.errorHandlerService.checkError(e, `WorkSchedule ${id}`);
+    }
   }
 }
