669bb632159d1645fab44716a1969639b1103023
MurAd <muradisayevazer@gmail.com>
2023-09-02 17:25:51 +0400

    helper imported

--- src/skills/skills.module.ts
+++ src/skills/skills.module.ts
@@ -3,9 +3,10 @@ import { SkillsService } from './skills.service';
 import { SkillsController } from './skills.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Skill } from './entities/skill.entity';
+import { HelperModule } from '../helper/helper.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Skill])],
+  imports: [TypeOrmModule.forFeature([Skill]), HelperModule],
   controllers: [SkillsController],
   providers: [SkillsService],
 })
