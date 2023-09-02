d5b809ca75a81be18dbe5c7f8986995649f87852
MurAd <muradisayevazer@gmail.com>
2023-09-02 22:24:04 +0400

    Experience Module Updated

--- src/experience/experience.module.ts
+++ src/experience/experience.module.ts
@@ -3,10 +3,11 @@ import { ExperienceService } from './experience.service';
 import { ExperienceController } from './experience.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Experience } from './entities/experience.entity';
-import { WorkSchedule } from 'src/work-schedule/entities/work-schedule.entity';
+import { WorkSchedule } from '../work-schedule/entities/work-schedule.entity';
+import { HelperModule } from '../helper/helper.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Experience, WorkSchedule])],
+  imports: [TypeOrmModule.forFeature([Experience, WorkSchedule]), HelperModule],
   controllers: [ExperienceController],
   providers: [ExperienceService],
 })
