9435fb2196522127198a41c13bb1b9bdc41d4441
MuradIsayev <muradisayevazer@gmail.com>
2024-01-24 17:33:47 +0400

    module was updated and notion service was added

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -1,21 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
-import { NotionModule } from 'nestjs-notion';
+// import { NotionModule } from 'nestjs-notion';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Blog } from './entities/blog.entity';
 import { HelperModule } from '../helper/helper.module';
+import { NotionService } from './notion.service';
 
 @Module({
-  imports: [
-    // TODO: Fix process.env issue
-    NotionModule.forRoot({
-      auth: process.env.NOTION_TOKEN,
-    }),
-    TypeOrmModule.forFeature([Blog]),
-    HelperModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Blog]), HelperModule],
   controllers: [BlogsController],
-  providers: [BlogsService],
+  providers: [BlogsService, NotionService],
 })
 export class BlogsModule {}
