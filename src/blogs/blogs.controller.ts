6d764eab589b38b8525753fd73c2c0e6bbfbddd3
MuradIsayev <muradisayevazer@gmail.com>
2024-01-30 01:46:42 +0400

    extra endpoints removed and viewCount endpoint added

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,29 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
-import { CreateBlogDto } from './dto/create-blog.dto';
-import { UpdateBlogDto } from './dto/update-blog.dto';
 
 @Controller('blog')
 export class BlogsController {
   constructor(private readonly blogsService: BlogsService) {}
-  @Get('search/')
-  findByTag(@Query('tag') tag: string) {
-    return this.blogsService.findFiltered(tag);
-  }
-
-  @Get('random')
-  findRandom() {
-    return this.blogsService.findRandom();
-  }
+  // @Get('search/')
+  // findByTag(@Query('tag') tag: string) {
+  //   return this.blogsService.findFiltered(tag);
+  // }
 
   @Get()
   findAll() {
@@ -35,23 +19,8 @@ export class BlogsController {
     return this.blogsService.getSinglePost(slug);
   }
 
-  @Get('content/:blockId')
-  findOne(@Param('blockId') blockId: string) {
-    return this.blogsService.findOne(blockId);
-  }
-
-  @Post()
-  create(@Body() createBlogDto: CreateBlogDto) {
-    return this.blogsService.create(createBlogDto);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
-    return this.blogsService.update(+id, updateBlogDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.blogsService.remove(+id);
+  @Patch('viewCount/:slug')
+  updateViewCount(@Param('slug') slug: string) {
+    return this.blogsService.updateViewCount(slug);
   }
 }
