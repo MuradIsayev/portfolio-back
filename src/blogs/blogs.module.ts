6e142a5b28324ec8ebd5a0cdcf7a03507fc26798
MuradIsayev <muradisayevazer@gmail.com>
2024-01-30 01:46:27 +0400

    Blogs removed

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
-// import { NotionModule } from 'nestjs-notion';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Blog } from './entities/blog.entity';
 import { HelperModule } from '../helper/helper.module';
 import { NotionService } from './notion.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Blog]), HelperModule],
+  imports: [HelperModule],
   controllers: [BlogsController],
   providers: [BlogsService, NotionService],
 })
--- src/config/db.entities.ts
+++ src/config/db.entities.ts
@@ -1,8 +1,6 @@
-import { Blog } from '../blogs/entities/blog.entity';
 import { Experience } from '../experience/entities/experience.entity';
 import { Project } from '../projects/entities/project.entity';
 import { Skill } from '../skills/entities/skill.entity';
-import { Tag } from '../tags/entities/tag.entity';
 import { WorkSchedule } from '../work-schedule/entities/work-schedule.entity';
 
-export const entities = [Skill, Project, Blog, Tag, Experience, WorkSchedule  ];
+export const entities = [Skill, Project, Experience, WorkSchedule];
